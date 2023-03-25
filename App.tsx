a15e75d5bdf84a9b2e5f937a2f0748479ff4810f
Farah <eduardobfarah@gmail.com>
2023-03-24 22:28:17 -0300

    Ignite Teams #3

--- App.tsx
+++ App.tsx
@@ -1,5 +1,27 @@
-import { Groups } from "@screens/Groups";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import theme from "@theme/index";
+
+import { ThemeProvider } from "styled-components";
+import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
+import { NewGroup } from "@screens/NewGroup";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <NewGroup /> : <Loading />}
+    </ThemeProvider>
+  );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,32 @@
-import { Container, Title } from "./styles";
+import { Button } from "@components/Button";
+import { GroupCard } from "@components/GroupCard";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Container } from "./styles";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
-      <Title>Groups</Title>
+      <Header />
+
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        keyExtractor={(item) => item}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        }
+      />
+
+      <Button title="Criar Nova Turma" />
     </Container>
   );
 }
