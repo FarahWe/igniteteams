88d71c9e13bbdbd821f727dc0d45f7d8fb5edcb9
Farah <eduardobfarah@gmail.com>
2023-03-26 11:13:27 -0300

    Implementando o voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,22 @@
 import { BackIcon, BackButton, Container, Logo } from "./styles";
 import logoImg from "@assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
   showBackButton?: boolean;
 };
 
 export function Header({ showBackButton = false }: Props) {
+  const { navigate } = useNavigation();
+
+  function handleGoBack() {
+    navigate("groups");
+  }
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
