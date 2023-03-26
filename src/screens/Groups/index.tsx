d1f505a5c1113f6f17a64e9bcef27ecfa2c1c976
Farah <eduardobfarah@gmail.com>
2023-03-26 11:00:24 -0300

    Tipando as rotas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,12 +3,19 @@ import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ListEmpty } from "@components/ListEmpty";
+import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
+  const { navigate } = useNavigation();
+
   const [groups, setGroups] = useState<string[]>([]);
+
+  function handleNewGroup() {
+    navigate("new");
+  }
 
   return (
     <Container>
@@ -27,7 +34,7 @@ export function Groups() {
         showsVerticalScrollIndicator={false}
       />
 
-      <Button title="Criar Nova Turma" />
+      <Button title="Criar Nova Turma" onPress={handleNewGroup} />
     </Container>
   );
 }
